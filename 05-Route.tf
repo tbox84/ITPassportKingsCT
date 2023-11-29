08f04b95f8fe4aa1962eb2687afdc1a2516af3e6
tbox84 <145474766+tbox84@users.noreply.github.com>
2023-11-29 12:17:31 -0500

    Update 05-Route.tf
    Needed to Add a Private and Public subnet

--- 05-Route.tf
+++ 05-Route.tf
@@ -60,6 +60,11 @@ resource "aws_route_table_association" "private-eu-west-1b" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_route_table_association" "private-eu-west-1c" {
+  subnet_id      = aws_subnet.private-eu-west-1c.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_route_table_association" "public-eu-west-1a" {
   subnet_id      = aws_subnet.public-eu-west-1a.id
   route_table_id = aws_route_table.public.id
@@ -67,5 +72,10 @@ resource "aws_route_table_association" "public-eu-west-1a" {
 
 resource "aws_route_table_association" "public-eu-west-1b" {
   subnet_id      = aws_subnet.public-eu-west-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-eu-west-1c" {
+  subnet_id      = aws_subnet.public-eu-west-1c.id
   route_table_id = aws_route_table.public.id
 }
